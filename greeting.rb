ab9dc50c583fe774ffa20d3fc9c05c4ed6bd5518
astronomical-reducer-9741 <vinh.v.tran@ucdenver.edu>
2020-02-10 21:14:10 +0000

    Automatically backed up by Learn

--- greeting.rb
+++ greeting.rb
@@ -1,3 +1,15 @@
-def greeting
-  puts "Hello World"
+def say_hello_world_ten_times
+  phrase = "Hello World!"
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
 end
+
+say_hello_world_ten_times
